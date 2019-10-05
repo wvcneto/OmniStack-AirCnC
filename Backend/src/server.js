da45b54872d3cf1baf61ef8dafe7c3c2859d9e64
Holfe <wvcneto@gmail.com>
2019-10-05 02:26:01 -0300

    Finish Web-Front

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -12,6 +13,7 @@ mongoose.connect('mongodb+srv://OmniStackDB:864717@omnistack-jae9j.mongodb.net/a
 
 app.use(cors()) //Local especifico origin: 'http://localhost:3333'
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..','uploads')));
 app.use(routes);
 
 app.listen(3333);
